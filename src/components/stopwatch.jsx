1a7045b9cf9853ce47341a9cd3b194f86fb27db8
AjayLiu <ajayliu2928@gmail.com>
2020-08-08 21:10:40 -0700

    netlify still aint workin

--- src/components/stopwatch.jsx
+++ src/components/stopwatch.jsx
@@ -38,7 +38,7 @@ class Stopwatch extends Component {
     let ms = ("0" + (timerTime % 1000)).slice(-3);
     // let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
     let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
-    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    // let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
     // let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
 
     let endMsg;
