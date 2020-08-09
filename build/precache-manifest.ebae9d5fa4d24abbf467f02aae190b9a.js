02e37ab9805f3dac9e20856139a6bef0f067266a
AjayLiu <ajayliu2928@gmail.com>
2020-08-08 20:56:25 -0700

    build

---
+++ build/precache-manifest.ebae9d5fa4d24abbf467f02aae190b9a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "28e300d277042f728f92",
+    "url": "/counter-app/static/css/main.1e582774.chunk.css"
+  },
+  {
+    "revision": "28e300d277042f728f92",
+    "url": "/counter-app/static/js/main.02484a45.chunk.js"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "6d35be40ec6747559afb",
+    "url": "/counter-app/static/css/2.03e98120.chunk.css"
+  },
+  {
+    "revision": "6d35be40ec6747559afb",
+    "url": "/counter-app/static/js/2.c0177fa7.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3042a92d8a93fffbf9efbee37a299752",
+    "url": "/counter-app/index.html"
+  }
+];
